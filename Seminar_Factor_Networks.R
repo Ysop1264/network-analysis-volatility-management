185a6a31c64331ffb9e4ca3f44ea62dfb48b29b8
YSop1264 <soppin.yadhu@gmail.com>
2026-03-24 10:32:20 +0100

    Add initial R script

---
+++ Seminar_Factor_Networks.R
@@ -0,0 +1,63 @@
+##### Uncomment if packages not installed
+# install.packages("tidyverse")
+# install.apckages("tidyfinance")
+# install.packages("scales")
+# install.packages("frenchdata")
+# install.packages("dplyr")
+# install.packages("moments")
+# install.packages("sandwich")
+# install.packages("lmtest")
+#####
+
+library(tidyverse)
+library(tidyfinance)
+library(scales)
+library(frenchdata)
+library(dplyr)
+library(moments)
+library(sandwich)
+library(lmtest)
+
+# DATA download and transformation
+start_date <- as.Date("1971-01-01")
+end_date   <- as.Date("2026-01-31")
+
+# Code from tidy-fianance website to download data
+factors_ff5_daily <- download_french_data("Fama/French 5 Factors (2x3) [Daily]")$subsets$data[[1]] |>
+  mutate(
+    date = ymd(date), 
+    across(c(RF, "Mkt-RF", SMB, HML, RMW, CMA), ~ as.numeric(.)/100), 
+    .keep = "none"
+  ) |> 
+  rename_with(str_to_lower) |>
+  rename(mkt_excess = "mkt-rf", risk_free = rf) |>
+  filter(date >= start_date & date <= end_date)
+
+momentum_daily <- download_french_data("Momentum Factor (Mom) [Daily]")$subsets$data[[1]] |>
+  mutate(
+    date = ymd(date),
+    across(Mom, ~ as.numeric(.)/100),
+    .keep = "none"
+  ) |>
+  rename_with(str_to_lower) |>
+  filter(date >= start_date & date <= end_date)
+
+industry_daily <- download_french_data("49 Industry Portfolios [Daily]")$subsets$data[[1]] |>
+  mutate(
+    date = ymd(date), 
+    across(-date, ~ as.numeric(.)/100), 
+    .keep = "unused"
+  ) |> 
+  rename_with(str_to_lower) |>
+  filter(date >= start_date & date <= end_date)
+
+factors_joined <- factors_ff5_daily |> left_join(momentum_daily, by = "date") |>
+  left_join(industry_daily, by = "date")
+
+factors_joined_excess <- factors_joined |> 
+  mutate(
+    across(-c(mkt_excess, smb, hml, rmw, cma, risk_free), ~ . - risk_free)
+  )
+
+managed_portfolios <- factors_joined_excess |> select(-risk_free)
+
